cd74f4c8c33566db6400b6ccb4882b64379145db
Archonic <gabrielc2904@gmail.com>
2025-02-27 11:01:43 -0500

    Create BinaryModulus.java

---
+++ src/BinaryModulus.java
@@ -0,0 +1,24 @@
+import java.util.BitSet;
+import java.util.Scanner;
+
+/**
+ * Binary Modulus
+ * Do modulus without %, /, *.
+ */
+
+public class BinaryModulus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers to mod:");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(modulus(a, b));
+    }
+
+    public static int modulus(int a, int b){
+        while((a - b) >= 0){
+            a -= b;
+        }
+        return a;
+    }
+}
