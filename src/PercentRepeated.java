482198424f6d3f88940a363a3c0c6a12bc9118ab
Archonic <gabrielc2904@gmail.com>
2025-04-02 09:08:24 -0400

    Update PercentRepeated.java

--- src/PercentRepeated.java
+++ src/PercentRepeated.java
@@ -6,7 +6,7 @@ public class PercentRepeated {
     //any repetition of a word after its first occurence is a "repeated word" what % of the total content is repeated?
     public static void main(String[] args) {
         String input = new Scanner(System.in).nextLine();
-        input = input.replace(",", "").replace(".", "").replace("?", "").replace("!", "").replace(":", "");
+        input = input.toLowerCase().replace(",", "").replace(".", "").replace("?", "").replace("!", "").replace(":", "");
         String[] words = input.split(" ");
         System.out.println(1.0 - ((double) (uniqueWords(words)) / words.length));
     }
